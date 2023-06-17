0e341c559041b6f04a43d8b94bf8ef69318fcb83
andTheowind <59265638+andTheowind@users.noreply.github.com>
2023-06-17 10:35:01 +0300

    Add files via upload

--- js/slider-script.js
+++ js/slider-script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(".logo-slider__owl-carousel").owlCarousel({
     loop: true, //Зацикливаем слайдер
-    nav: true, //Отключение навигации
+    nav: false, //Отключение навигации
     autoplay: false, //Автозапуск слайдера
     smartSpeed: 1000, //Время движения слайда
     autoplayTimeout: 2000, //Время смены слайда
@@ -28,27 +28,3 @@ $(document).ready(function () {
   }
 });
 
-
-
-
-
-
-// Добавляем стрелки на первый и последний слайд
-$(".logo-slider__owl-carousel").on("initialized.owl.carousel resized.owl.carousel", function(event) {
-  var $carousel = $(event.target);
-  var $firstSlide = $carousel.find(".owl-item:first-child");
-  var $lastSlide = $carousel.find(".owl-item:last-child");
-
-  $firstSlide.toggleClass("owl-item--first", $firstSlide.hasClass("active"));
-  $lastSlide.toggleClass("owl-item--last", $lastSlide.hasClass("active"));
-});
-
-// Обновляем классы при переключении слайдов
-$(".logo-slider__owl-carousel").on("changed.owl.carousel", function(event) {
-  var $carousel = $(event.target);
-  var $firstSlide = $carousel.find(".owl-item:first-child");
-  var $lastSlide = $carousel.find(".owl-item:last-child");
-
-  $firstSlide.toggleClass("owl-item--first", $carousel.find(".owl-item.active:first").is($firstSlide));
-  $lastSlide.toggleClass("owl-item--last", $carousel.find(".owl-item.active:last").is($lastSlide));
-});
